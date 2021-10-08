d6bd07f0952d0bbc0128f8b80f11ee52e8e80ab4
DiBDV <bdv.dmitry@gmail.com>
2021-10-08 22:35:19 +0200

    Add supertest package

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+  res.send('Hello Stranger');
+});
+
+app.get('/helloworld', (req, res) => {
+  res.send('Hello Stranger');
+});
+
+app.get('/helloworld?name=AlfredENeumann', (req, res) => {
+  res.send('Hello Alfred E Neumann');
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
